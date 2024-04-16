305e166bc854e119dabfad2089b5ef97d8c2d82a
EduSG <eduardo.gomes.072020@gmail.com>
2024-04-15 22:56:54 -0300

    protect

---
+++ painel.php
@@ -0,0 +1,18 @@
+<?php
+if(isset($_SESSION)){
+    session_start();
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Bem vindo ao painel <?php echo $_SESSION['nome']?></h1>
+</body>
+</html>
