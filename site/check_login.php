bbf4dc79ff855bb460febfb11bd4c3c0bb54fc4c
EduSG <eduardo.gomes.072020@gmail.com>
2024-04-29 18:00:53 -0300

    resposivo sobre

--- site/check_login.php
+++ site/check_login.php
@@ -7,27 +7,57 @@ error_reporting(E_ALL);
 require 'conexao.php';
 $conn = connectionFactory::conexaoMysqli();
 
-if (!$conn) {
-    echo json_encode(["error" => "A conexão com o banco de dados falhou: " . mysqli_error()]);
-    exit;
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
 }
 
-if (isset($_POST['user_input'])) {
+header('Content-Type: application/json'); // Define a resposta como JSON
+
+
+if (isset($_POST['user_input']) || isset($_POST['password'])) {
+
+    if (strlen($_POST['user_input']) == 0) {
+        echo json_encode(['error' => 'Preencha seu e-mail']);
+        exit;
+    }
+    if (strlen($_POST['password']) == 0) {
+        echo json_encode(['error' => 'Preencha sua senha']);
+        exit;
+    }
+
     $username = mysqli_real_escape_string($conn, $_POST['user_input']);
     $password = mysqli_real_escape_string($conn, $_POST['password']);
+
 
     if (empty($username) || empty($password)) {
         echo json_encode(["error" => "Por favor, insira um nome de usuário e senha válidos!"]);
         exit;
     }
 
-    $query = "SELECT `senha` FROM `usuario` WHERE `nome` = '$username'";
-    $result = mysqli_query($conn, $query);
+    $query = $conn->prepare("SELECT * FROM `usuario` WHERE `usuario` = ? AND senha = ?");
+    $query->bind_param('ss', $username, $password);
 
-    if (mysqli_num_rows($result) == 1) {
+    $query->execute();
+
+    $result = $query->get_result();
+    if ($result->num_rows == 1) {
+        $user = $result->fetch_assoc();
+
+        if (!isset($_SESSION)) {
+            session_start();
+        }
+
+        $_SESSION['id'] = $user['id_usuario'];
+        $_SESSION['name'] = $user['usuario'];
+
+        echo json_encode(['success' => 'Login realizado com sucesso.']);
+    } else {
+        echo json_encode(['error' => 'Usuário ou senha incorretos.']);
 
     }
 
+} else {
+    echo json_encode(['error' => 'Falha ao logar, verifique os dados!']);
 }
 
 
--- site/src/js/script.js
+++ site/src/js/script.js
@@ -15,47 +15,89 @@ see_pass.addEventListener('click', () => {
     see_pass.checked ? pass.type = 'text' : pass.type = 'password';
 })
 
-class ValidaFormulario{
-    constructor(){
-        this.btn_submit = document.querySelector('#submit_btn');
-        this.user = document.querySelector('#user_cred');
-        this.pass = pass;
-        this.events();
-        console.log('click')
-
-    }
-
-    events(){
-        this.btn_submit.addEventListener('click', () => {
-            this.check_lenght();
-            console.log('click')
-        });
-    }
-
-    remove_all(){
-        for(let errorText of this.formulario.querySelectorAll('.error-text')) {
-            errorText.remove();
-        }
-    }
-
-    check_lenght(){
-        if(!this.user.value){
-            this.criaErro(this.user, `O campo usuário não pode estar vazio!`)
-        }
-        if(!this.pass.value){
-            this.criaErro(this.pass, `O campo senha não pode estar vazio!`)
-        }
-    }
-
-
-    criaErro(campo, msg) {
-        const div = document.createElement('div');
-        div.innerHTML = msg;
-        div.classList.add('error-text');
-        campo.classList.add('error-input')
-        campo.insertAdjacentElement('afterend', div);
-        return;
-    }
+function errors(){
+    
 }
 
-const valida = new ValidaFormulario();
+document.querySelector('#submit_btn').addEventListener('click', (e) => {
+    e.preventDefault();
+    let form_data = new FormData();
+
+    const user = document.querySelector('#user_cred_in')
+    const pass = document.querySelector('#pass_cred_in')
+
+    
+    form_data.append("user_input", user.value);
+    form_data.append("password", pass.value);
+
+    console.log(form_data);
+
+    console.log('enviado')
+    fetch('check_login.php', {
+        method: 'POST',
+        body: form_data,
+    })
+    .then(response => response.json()) // Processa como JSON
+    .then(data => {
+        if (data.error) {
+            console.error('Erro:', data.error); // Mostra erros
+            
+        } else {
+            console.log('Sucesso:', data.success); // Pode redirecionar ou fazer outras ações
+        }
+    })
+    .catch(error => console.error('Erro no fetch:', error));
+
+});
+
+
+
+
+
+
+
+
+// class ValidaFormulario{
+//     constructor(){
+//         this.btn_submit = document.querySelector('#submit_btn');
+//         this.user = document.querySelector('#user_cred');
+//         this.pass = pass;
+//         this.events();
+//         console.log('click')
+
+//     }
+
+//     events(){
+//         this.btn_submit.addEventListener('click', () => {
+//             this.check_lenght();
+//             console.log('click')
+//         });
+//     }
+
+//     remove_all(){
+//         for(let errorText of this.formulario.querySelectorAll('.error-text')) {
+//             errorText.remove();
+//         }
+//     }
+
+//     check_lenght(){
+//         if(!this.user.value){
+//             this.criaErro(this.user, `O campo usuário não pode estar vazio!`)
+//         }
+//         if(!this.pass.value){
+//             this.criaErro(this.pass, `O campo senha não pode estar vazio!`)
+//         }
+//     }
+
+
+//     criaErro(campo, msg) {
+//         const div = document.createElement('div');
+//         div.innerHTML = msg;
+//         div.classList.add('error-text');
+//         campo.classList.add('error-input')
+//         campo.insertAdjacentElement('afterend', div);
+//         return;
+//     }
+// }
+
+// const valida = new ValidaFormulario();
